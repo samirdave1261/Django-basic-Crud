cd7cfb72a1237947a9c31e5cf97d361b51244c40
Samir Innovation (Intellial) <113102510+samirdave1261@users.noreply.github.com>
2022-09-28 18:23:03 +0530

    Update admin.py

--- samirDjango/crud/admin.py
+++ samirDjango/crud/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import Member
 
 # Register your models here.
+admin.site.site_header  =  "Samir-Dave Crud Admin" 
+admin.site.index_title  =  "Samir-Dave Crud Admin"
 
 admin.site.register(Member)
